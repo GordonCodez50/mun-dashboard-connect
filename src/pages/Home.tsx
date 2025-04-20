ee18634f21194b8d3e0c375bee5305bc460707da
Gordon Godwin Edacheril <gordongedacheril1@gmail.com>
2025-04-20 18:05:43 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
               <h1 className="text-4xl font-semibold text-black dark:text-white">
                 Welcome to <br />
                 <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                  ISBMUN 2024
+                  ISBMUN 2025
                 </span>
               </h1>
             </>
